2eaeaa21636d442ec49b43c43c50967dba1925c2
Дима Касянюк <kasyanyuk.d1995@gmail.com>
2020-07-30 12:37:29 +0300

    create list is succes

--- lib/pages/list_cities_page.dart
+++ lib/pages/list_cities_page.dart
@@ -1,6 +1,7 @@
 import 'package:CityMap/models/city_model.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:CityMap/widgets/item_city.dart';
+import 'package:flutter/material.dart';
 
 class CityList extends StatelessWidget {
   final List<CityModel> listCity = [
@@ -12,35 +13,104 @@ class CityList extends StatelessWidget {
     CityModel(
       id: 2,
       name: "London",
-      description: "City of Jopa",
+      description: "London is the capital of Great Britain",
     ),
     CityModel(
       id: 3,
-      name: "New York",
-      description: "City of 12a",
+      name: "Berlin",
+      description: "Berlin is the capital of Germany",
     ),
     CityModel(
       id: 4,
-      name: "Brest",
-      description: "City of Love",
+      name: "Malorita",
+      description: "My wife was born in Malorita",
     ),
     CityModel(
       id: 5,
-      name: "Vitebsk",
-      description: "slav bazar",
+      name: "Lyakhovtsy",
+      description: "Lyakhovtsy is my native village",
+    ),
+    CityModel(
+      id: 6,
+      name: "Baku",
+      description: "Baku is the capital of Azerbaijan",
+    ),
+    CityModel(
+      id: 7,
+      name: "Ottawa",
+      description: "Ottawa is the capital of Canada",
+    ),
+    CityModel(
+      id: 8,
+      name: "Paris",
+      description: "Paris is the capital of France",
+    ),
+    CityModel(
+      id: 9,
+      name: "Tbilisi",
+      description: "Tbilisi is the capital of Georgia",
+    ),
+    CityModel(
+      id: 10,
+      name: "Jakarta",
+      description: "Jakarta is the capital of Indonesia",
+    ),
+    CityModel(
+      id: 11,
+      name: "Rome",
+      description: "Rome is the capital of Italy",
+    ),
+    CityModel(
+      id: 12,
+      name: "Tokyo",
+      description: "Tokyo is the capital of Japan",
+    ),
+    CityModel(
+      id: 13,
+      name: "Astana",
+      description: "Astana is the capital of Kazakhstan",
+    ),
+    CityModel(
+      id: 14,
+      name: "Riga",
+      description: "Riga is the capital of Latvia",
+    ),
+    CityModel(
+      id: 15,
+      name: "Rabat",
+      description: "Rabat is the capital of Morocco",
+    ),
+    CityModel(
+      id: 16,
+      name: "Oslo",
+      description: "Oslo is the capital of Norway",
     ),
   ];
 
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        final itemModel = listCity[index];
-        return ItemCity(
-          name: itemModel.name,
-          description: itemModel.description,
-        );
-      },
-      itemCount: listCity.length,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Cities",
+        ),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final itemModel = listCity[index];
+          return ItemCity(
+            name: itemModel.name,
+            description: itemModel.description,
+          );
+        },
+        itemCount: listCity.length,
+      ),
+      backgroundColor: Colors.white,
+      drawer: Column(
+        children: <Widget>[
+          Text("safsf"),
+          Text("dfd"),
+        ],
+      ),
     );
   }
 }
--- lib/widgets/item_city.dart
+++ lib/widgets/item_city.dart
@@ -13,25 +13,33 @@ class ItemCity extends StatelessWidget {
   });
 
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Text(
-          name,
-          textAlign: TextAlign.right,
-          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(9, 12, 3, 3),
+      child: Align(
+        alignment: Alignment.topLeft,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              name.toUpperCase(),
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.black,
+                fontStyle: FontStyle.normal,
+                //fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              description,
+              style: TextStyle(
+                fontSize: 15.0,
+                color: Colors.blueGrey,
+              ),
+            ),
+          ],
         ),
-        SizedBox(
-          height: 5.0,
-        ),
-        Text(
-          description,
-          textAlign: TextAlign.left,
-          style: TextStyle(
-            fontSize: 20.0,
-            color: Colors.grey,
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
